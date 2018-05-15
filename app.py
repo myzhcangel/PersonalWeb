a139bb6f363254478d2a4e26f2ca9689e17b46e3
myzhcangel <chengminke13@gmail.com>
2018-05-15 17:07:50 -0400

    add email by zhc

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template
+from sparkpost import SparkPost
 
 conn = None
 app = Flask(__name__)
@@ -15,6 +16,19 @@ def experience():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/email')
+def send_email():
+	sparky = SparkPost('b5db5f3dbabccc1a978befe88ca226807b695e24')
+ 
+	response = sparky.transmissions.send(
+	    use_sandbox=True,
+	    recipients=['developers+python@sparkpost.com'],
+	    html='<html><body><p>Testing SparkPost - the world\'s most awesomest email service!</p></body></html>',
+	    from_email='testing@sparkpostbox.com',
+	    subject='Oh hey!')
+
+	return render_template('about.html')
 
 if __name__ == "__main__":
 	app.run()
